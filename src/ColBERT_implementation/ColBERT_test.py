ac461a058f88400963e26f4a12ebfb6101f9940d
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2023-12-13 20:50:16 +0200

    stuck on runtime error

--- src/ColBERT_implementation/ColBERT_test.py
+++ src/ColBERT_implementation/ColBERT_test.py
@@ -1,13 +1,22 @@
 import os
-
 from colbert.infra import Run, RunConfig, ColBERTConfig
 from colbert.data import Queries, Collection
 from colbert import Indexer, Searcher
 
 queries = os.path.join('data/tsv/queries.tsv')
 collection = os.path.join('data/tsv/docs.tsv')
+checkpoint = os.path.join('ColBERT_implementation/checkpoint/colbertv2.0')
+index_name = "cfcolection.nbits=2"
+
 queries = Queries(path=queries)
 collection = Collection(path=collection)
-
 print (f'Loaded {len(queries)} queries and {len(collection):,} passages')
 
+
+with Run().context(RunConfig(nranks=1, experiment="cfcollection")):
+    config = ColBERTConfig(
+        nbits=2,
+        root="/experiments",
+    )
+    indexer = Indexer(checkpoint=checkpoint, config=config)
+    indexer.index(name=index_name, collection=collection, overwrite=True)
