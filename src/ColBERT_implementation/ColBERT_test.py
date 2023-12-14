19f897678ed103a9bce687f8f371dfd9846843ff
ptriantafy <pantrian04@gmail.com>
2023-12-14 19:26:41 +0200

    stuck on cuda error on runtime

--- src/ColBERT_implementation/ColBERT_test.py
+++ src/ColBERT_implementation/ColBERT_test.py
@@ -5,18 +5,18 @@ from colbert import Indexer, Searcher
 
 queries = os.path.join('data/tsv/queries.tsv')
 collection = os.path.join('data/tsv/docs.tsv')
-checkpoint = os.path.join('ColBERT_implementation/checkpoint/colbertv2.0')
+checkpoint = os.path.join('src/ColBERT_implementation/checkpoint/colbertv2.0')
 index_name = "cfcolection.nbits=2"
 
 queries = Queries(path=queries)
 collection = Collection(path=collection)
 print (f'Loaded {len(queries)} queries and {len(collection):,} passages')
 
-
-with Run().context(RunConfig(nranks=1, experiment="cfcollection")):
-    config = ColBERTConfig(
-        nbits=2,
-        root="/experiments",
-    )
-    indexer = Indexer(checkpoint=checkpoint, config=config)
-    indexer.index(name=index_name, collection=collection, overwrite=True)
+if __name__ == '__main__': #without this it won't run...
+    with Run().context(RunConfig(nranks=1, experiment="cfcollection")):
+        config = ColBERTConfig(
+            nbits=2,
+            root="/experiments",
+        )
+        indexer = Indexer(checkpoint=checkpoint, config=config)
+        indexer.index(name=index_name, collection=collection, overwrite=True)
